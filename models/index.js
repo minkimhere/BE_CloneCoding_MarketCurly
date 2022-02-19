d4e33487eab8a17d5fa65ae2fa42b50b823dfbc5
minkimhere <mj.kim.tech@gmail.com>
2022-02-19 19:49:17 +0900

    몽고디비 서버에러 해결중

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/market-curly')
-    .catch((error) => {
-      console.error(error);
-    });
+     .connect(process.env.MONGO_URL)
+    .catch(err => console.log(err));
 };
+
+mongoose.connection.on("error", err => {
+  console.error("몽고디비 연결 에러", err);
+});
 
 module.exports = connect;
