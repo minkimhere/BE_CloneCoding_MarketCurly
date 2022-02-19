60efbd28e4a5e141ff76dd227ef12b5cb4193455
minkimhere <mj.kim.tech@gmail.com>
2022-02-19 21:47:36 +0900

    몽고디비서버 해결중

--- models/index.js
+++ models/index.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-     .connect(process.env.MONGO_URL)
-    .catch(err => console.log(err));
+    .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/market-curly', {
+      dbName: 'market-curly',
+      ignoreUndefined: true,
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 mongoose.connection.on("error", err => {
