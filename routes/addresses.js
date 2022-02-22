caa1a269fc04198255175cfac84ae93133022fc8
minkimhere <mj.kim.tech@gmail.com>
2022-02-22 15:15:13 +0900

    오타 발견 수정

--- routes/addresses.js
+++ routes/addresses.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/auth-middleware')
 const { postAddress, getAddress } = require('../controller/addresses')
 
-const { postAddress, getAddress} = require('../controller/addresses'); 
-
 // 주소 추가
 router.post('/:userId', authMiddleware, postAddress);
 
