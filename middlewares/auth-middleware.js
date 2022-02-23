ac04636014de9d0077cf854b48c0551a6c892bf6
Ausdauer1 <steady051@naver.com>
2022-02-23 18:40:50 +0900

    feat02223

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,35 +1,29 @@
-const jwt = require("jsonwebtoken");
-const Users = require("../models/users");
+const jwt = require('jsonwebtoken');
+const Users = require('../schemas/users');
 
+// 인증
 module.exports = (req, res, next) => {
-  // authoriztion 참조
-  const { authorization } = req.headers; // 프론트에서 대문자로 보내도 여기서는 소문자로 변환됨
-  const [tokenType, tokenValue] = authorization.split(" "); // 공백을 기준으로 배열을 반환 // Bearer랑 외계어 사이에 공백있음.
+  const { authorization } = req.headers;
+  const [authType, authToken] = (authorization || '').split(' ');
 
-  if (!tokenValue) {
-    res.locals.users = null;
-    next();
-    return;
-  }
-
-  if (tokenType !== "Bearer") {
+  // 인증 Bearer 타입 아니면 거르기
+  if (!authToken || authType !== 'Bearer') {
     res.status(401).send({
-      errorMessage: "로그인 후 사용하세요",
+      errorMessage: '로그인 후 이용 가능한 기능입니다.',
     });
     return;
   }
+
+  // 로그인정보 저장해놓기
   try {
-    const { userId } = jwt.verify(tokenValue, process.env.TOKENKEY); // 시크릿키로 검증해서 유효하다면 디코딩 값 넣어주기.(users.js에서 userId로 인코딩했으므로 여기서도 userId로 디코딩)
-    Users.findById(userId)
-      .exec()
-      .then((user) => {
-        res.locals.user = user; //locals에 유저 정보 저장
-        next(); // 다음 미들웨어 실행하기 // next 안하면 미들웨어에서 에러처리에 걸림
-      });
-  } catch (error) {
-    res.status(401).send({
-      errorMessage: "로그인 후 사용하세요",
+    const { userId } = jwt.verify(authToken, process.env.TOKENKEY);
+    Users.findById(userId).then((user) => {
+      res.locals.user = user; //굳이 데이터베이스에서 사용자 정보를 가져오지 않게 할 수 있도록 express가 제공하는 안전한 변수에 담아두고 언제나 꺼내서 사용할 수 있게함
+      next();
     });
-    return;
+  } catch (err) {
+    res.status(401).send({
+      errorMessage: '로그인 후 이용 가능한 기능입니다.',
+    });
   }
 };
