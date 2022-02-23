cc96aef2c7c2cac13eb4068b6b6a34034ef4f6bb
nklee6300 <nogi3001@gmail.com>
2022-02-23 11:57:59 +0900

    장바구니 ud 수정본

--- controller/carts.js
+++ controller/carts.js
@@ -3,15 +3,13 @@ const Posts = require('../models/pages');
 
 const postCart = async (req, res) => {
     const userEmail = res.locals.user.email;
-    // console.log(userEmail)
     const { postId, quantity } = req.body;
     const thePost = await Posts.findOne({postId});
-
     const exitCart = await Cart.findOne({ userId: userEmail, postId });
     console.log(exitCart)
     if(exitCart){
       await Cart.updateOne({ userId : userEmail, postId }, { $inc: { quantity:+quantity }});
-      return res.status(200).json({ ok: 'true22' })
+      return res.status(200).json({ ok: 'true' })
     } 
     try{
       await Cart.create({
@@ -23,7 +21,6 @@ const postCart = async (req, res) => {
         quantity,
       });
       const data = { userId : userEmail, postId, title: thePost.title, price: thePost.price, img : thePost.img, quantity};
-      // console.log(data)
       res.status(200).json({ ok: 'true', data });
     } catch (error) {
       res.status(400).json({ ok: 'false' });
@@ -31,10 +28,9 @@ const postCart = async (req, res) => {
 };
 
 const getCart = async (req, res) => {
-  const userId = res.locals.user.userId;
-  console.log(userId, '유저, 포스트 아이디 확인용');
+  const userEmail = res.locals.user.email;
   try{
-    const getCarts = await Cart.find({ userId });
+    const getCarts = await Cart.find({ userEmail });
       return res.status(200).json({ 
         ok: 'true',
         carts: getCarts
@@ -45,7 +41,7 @@ const getCart = async (req, res) => {
 };
 
 const putCart = async (req, res) => {
-  const userId = res.locals.user.userId;
+  const userEmail = res.locals.user.email;
   const { postId } = req.params;
   const { quantity } = req.body;
 
@@ -61,8 +57,8 @@ const putCart = async (req, res) => {
   // }
 
   try{
-    await Cart.updateOne({ userId, postId }, { $set: { quantity }});
-    const putCarts = await Cart.find({ userId, postId });
+    await Cart.updateOne({ userEmail, postId }, { $set: { quantity }});
+    const putCarts = await Cart.find({ userEmail, postId });
     res.status(200).send({ ok: 'true', message: '수정 성공', putCarts});
   } catch (error) {
     res.status(400).json({ ok: 'false', message: '수정 실패'});
@@ -70,12 +66,12 @@ const putCart = async (req, res) => {
 };
 
 const deleteCart = async (req, res) => {
-  const userId = res.locals.user.userId;
+  const userEmail = res.locals.user.email;
   const { postId } = req.params;
   try{
-    const deleteCarts = await Cart.find({ userId, postId });
+    const deleteCarts = await Cart.find({ userEmail, postId });
     if(deleteCarts.length) {
-      await Cart.deleteOne({ userId, postId });
+      await Cart.deleteOne({ userEmail, postId });
     }
     res.status(200).json({ ok: 'true', message: '삭제 성공' });
   } catch (error) {
