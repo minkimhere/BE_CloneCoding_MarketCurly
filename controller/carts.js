588c64e4890b84e05f54dacde1559b0023f83e61
minkimhere <mj.kim.tech@gmail.com>
2022-02-23 14:03:41 +0900

    장바구니 수량 증가, 감소 추가

--- controller/carts.js
+++ controller/carts.js
@@ -1,42 +1,52 @@
-const Cart = require('../models/carts');
-const Posts = require('../models/pages');
+const Cart = require("../models/carts");
+const Posts = require("../models/pages");
 
 const postCart = async (req, res) => {
-    const userEmail = res.locals.user.email;
-    const { postId, quantity } = req.body;
-    const thePost = await Posts.findOne({postId});
-    const exitCart = await Cart.findOne({ userId: userEmail, postId });
-    console.log(exitCart)
-    if(exitCart){
-      await Cart.updateOne({ userId : userEmail, postId }, { $inc: { quantity:+quantity }});
-      return res.status(200).json({ ok: 'true' })
-    } 
-    try{
-      await Cart.create({
-        userEmail,
-        postId,
-        title: thePost.title,
-        price: thePost.price,
-        img : thePost.img,
-        quantity,
-      });
-      const data = { userId : userEmail, postId, title: thePost.title, price: thePost.price, img : thePost.img, quantity};
-      res.status(200).json({ ok: 'true', data });
-    } catch (error) {
-      res.status(400).json({ ok: 'false' });
-    }
+  const userEmail = res.locals.user.email;
+  const { postId, quantity } = req.body;
+  const thePost = await Posts.findOne({ postId });
+  const exitCart = await Cart.findOne({ userId: userEmail, postId });
+  console.log(exitCart);
+  if (exitCart) {
+    await Cart.updateOne(
+      { userId: userEmail, postId },
+      { $inc: { quantity: +quantity } }
+    );
+    return res.status(200).json({ ok: "true" });
+  }
+  try {
+    await Cart.create({
+      userEmail,
+      postId,
+      title: thePost.title,
+      price: thePost.price,
+      img: thePost.img,
+      quantity,
+    });
+    const data = {
+      userId: userEmail,
+      postId,
+      title: thePost.title,
+      price: thePost.price,
+      img: thePost.img,
+      quantity,
+    };
+    res.status(200).json({ ok: "true", data });
+  } catch (error) {
+    res.status(400).json({ ok: "false" });
+  }
 };
 
 const getCart = async (req, res) => {
   const userEmail = res.locals.user.email;
-  try{
+  try {
     const getCarts = await Cart.find({ userEmail });
-      return res.status(200).json({ 
-        ok: 'true',
-        carts: getCarts
-       });
+    return res.status(200).json({
+      ok: "true",
+      carts: getCarts,
+    });
   } catch (error) {
-    res.status(400).json({ ok: 'false' });
+    res.status(400).json({ ok: "false" });
   }
 };
 
@@ -56,26 +66,72 @@ const putCart = async (req, res) => {
   //   });
   // }
 
-  try{
-    await Cart.updateOne({ userEmail, postId }, { $set: { quantity }});
+  try {
+    await Cart.updateOne({ userEmail, postId }, { $set: { quantity } });
     const putCarts = await Cart.find({ userEmail, postId });
-    res.status(200).send({ ok: 'true', message: '수정 성공', putCarts});
+    res.status(200).send({ ok: "true", message: "수정 성공", putCarts });
   } catch (error) {
-    res.status(400).json({ ok: 'false', message: '수정 실패'});
+    res.status(400).json({ ok: "false", message: "수정 실패" });
+  }
+};
+
+// 장바구니 수량 증가 +1
+const putCartInc = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const loginUserEmail = res.locals.user.email; // 로그인 정보에 담아놓은 email
+    const user = await Cart.findOne({ postId }); // 디비에 있는 email
+
+    if (user.userEmail === loginUserEmail) { // 로그인 된 유저일 경우만
+      await Cart.updateOne({ postId }, { $inc: { quantity: +1 } });
+      res.json({ ok: true, message: "장바구니 수량 증가 +1 완료" });
+    } else {
+      res.json({ ok: false, message: "장바구니 수량 증가 +1 실패" });
+    }
+  } catch (error) {
+    res.status(400).json({ ok: false, message: "장바구니 수량 증가 +1 실패" });
+    console.error(`장바구니 수량 증가에서 ${error}에러가 발생하였습니다.`);
+  }
+};
+
+// 장바구니 수량 감소 -1
+const putCartDec = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const loginUserEmail = res.locals.user.email; // 로그인 정보에 담아놓은 email
+    const user = await Cart.findOne({ postId }); // 디비에 있는 email
+    const quantity = user.quantity; // 장바구니 수량
+
+    // 1 이하는 감소 불가
+    if (quantity === 1) {
+      return res.json({
+        ok: false,
+        message: "삭제를 원하시면 삭제 버튼을 눌러주세요.",
+      });
+    }
+
+    if (user.userEmail === loginUserEmail) { // 로그인 된 유저일 경우만
+      await Cart.updateOne({ postId }, { $inc: { quantity: -1 } });
+      res.json({ ok: true, message: "장바구니 수량 감소 -1 성공" });
+    } else {
+      res.json({ ok: false, message: "장바구니 수량 감소 -1 실패" });
+    }
+  } catch (error) {
+    res.status(400).json({ ok: false, message: "장바구니 수량 감소 -1 실패" });
   }
 };
 
 const deleteCart = async (req, res) => {
   const userEmail = res.locals.user.email;
   const { postId } = req.params;
-  try{
+  try {
     const deleteCarts = await Cart.find({ userEmail, postId });
-    if(deleteCarts.length) {
+    if (deleteCarts.length) {
       await Cart.deleteOne({ userEmail, postId });
     }
-    res.status(200).json({ ok: 'true', message: '삭제 성공' });
+    res.status(200).json({ ok: "true", message: "삭제 성공" });
   } catch (error) {
-    res.status(400).json({ ok: 'false', message: '삭제 실패' });
+    res.status(400).json({ ok: "false", message: "삭제 실패" });
   }
 };
 
@@ -83,5 +139,7 @@ module.exports = {
   postCart,
   getCart,
   putCart,
+  putCartInc,
+  putCartDec,
   deleteCart,
 };
