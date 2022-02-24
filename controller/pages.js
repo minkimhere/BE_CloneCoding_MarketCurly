16d921ea014e02e1d133c42e37559d9599c92069
Ausdauer1 <steady051@naver.com>
2022-02-24 11:08:07 +0900

    feat0224(1)

--- controller/pages.js
+++ controller/pages.js
@@ -3,8 +3,14 @@ const Posts = require('../models/pages');
 // 메인페이지로 줄 정보 
 exports.mainitems = async (req, res) => {
   try {
-    const sortByNew = await Posts.find().exec();
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+    
+    // const sortByNew = await Posts.find().exec();
+    const sortByNew = paginatedResults(page, limit, Posts)
+
     res.json({ ok:'true', sortByNew });
+    
   } catch (error) {
     res.status(400).json({ ok: 'false'})
   } 
@@ -31,3 +37,32 @@ exports.discountitems = async (req, res) => {
     res.status(400).json({ ok: 'false'})
   }  
 };
+
+function paginatedResults(page, limit, model) {
+  return async (req, res) => {
+  
+      const startIndex = (page -1) * limit
+      const endIndex = page * limit
+  
+      const results = {}
+      
+      if (endIndex < await model.countDocuments().exec()) {
+          results.next = {
+              page: page +1,
+              limit: limit
+          }
+      }
+      if (startIndex > 0) {
+          results.previous = {
+              page: page -1,
+              limit: limit
+          }
+      }
+      try {
+          results.results = await model.find().limit(limit).skip(startIndex).exec()  
+          res.paginatedResults = results 
+      } catch (e) {
+          res.status(500).json({ok: 'false2'})
+      }
+  }
+}
