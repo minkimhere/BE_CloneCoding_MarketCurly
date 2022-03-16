3f207b96d72bc962de5f73feb7563e97eb578b62
Ausdauer1 <steady051@naver.com>
2022-03-16 16:32:37 +0900

    3.16.1

--- controller/pages.js
+++ controller/pages.js
@@ -1,14 +1,14 @@
-const Posts = require('../models/pages');
+const Posts = require('../models');
+
 
 // 메인페이지로 줄 정보 
 exports.mainitems = async (req, res) => {
   try {
     const page = parseInt(req.query.page)
     const limit = parseInt(req.query.limit)
-    
-    // const sortByNew = await Posts.find().exec();
-    const sortByNew = paginatedResults(page, limit, Posts)
 
+    const sortByNew = await paginatedResults(page, limit, Posts)
+    
     res.json({ ok:'true', sortByNew });
     
   } catch (error) {
@@ -18,10 +18,34 @@ exports.mainitems = async (req, res) => {
 // 베스트 페이지로 줄 정보 (랜덤 정렬)
 exports.bestitems = async (req, res) => {
   try {
-    const sortByBest = await Posts.find().exec();
+    const Allposts = await Posts.find().exec();
+    
     //가격 낮은순
     // sortByBest.sort((a,b) => Number(a.price.replace(/,|원/gi,'')) - Number(b.price.replace(/,|원/gi,'')))
-    sortByBest.sort(()=> Math.random() - 0.5);
+
+    Allposts.sort(()=> Math.random() - 0.5);
+
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
+    const startIndex = (page -1) * limit
+    const endIndex = page * limit
+
+    const sortByBest = {};
+    if (endIndex < Allposts.length) {
+          sortByBest.next = {
+              page: page +1,
+              limit: limit
+          }
+      }
+      if (startIndex > 0) {
+          sortByBest.previous = {
+              page: page -1,
+              limit: limit
+          }
+      }
+      sortByBest.results = Allposts.splice(startIndex, limit)
+
     res.json({ ok:'true', sortByBest });
   } catch (error) {
     res.status(400).json({ ok: 'false'})
@@ -38,31 +62,28 @@ exports.discountitems = async (req, res) => {
   }  
 };
 
-function paginatedResults(page, limit, model) {
-  return async (req, res) => {
+
+async function paginatedResults(page, limit, model) {
+
+  const startIndex = (page -1) * limit
+  const endIndex = page * limit
   
-      const startIndex = (page -1) * limit
-      const endIndex = page * limit
+  const results = {}
   
-      const results = {}
-      
-      if (endIndex < await model.countDocuments().exec()) {
-          results.next = {
-              page: page +1,
-              limit: limit
-          }
-      }
-      if (startIndex > 0) {
-          results.previous = {
-              page: page -1,
-              limit: limit
-          }
-      }
-      try {
-          results.results = await model.find().limit(limit).skip(startIndex).exec()  
-          res.paginatedResults = results 
-      } catch (e) {
-          res.status(500).json({ok: 'false2'})
+  if (endIndex < await model.countDocuments().exec()) {
+      results.next = {
+          page: page +1,
+          limit: limit
       }
   }
+  if (startIndex > 0) {
+      results.previous = {
+          page: page -1,
+          limit: limit
+      }
+  }
+  
+  results.results = await model.find().limit(limit).skip(startIndex).exec() 
+  return results 
+      
 }
